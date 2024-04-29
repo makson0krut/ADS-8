f9ca295ebf06caa00937b0e55fab848f58ba90b7
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-04-29 10:40:43 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -10,7 +10,7 @@ class Train {
     Cage *prev;
     Cage(bool light, Cage* prev) :light(light), prev(prev) {}
   };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
+  int countOp = 0; // счетчик шагов (число переходов из вагона в вагон)
   Cage *first; // точка входа в поезд (первый вагон)
  public:
   Train();
